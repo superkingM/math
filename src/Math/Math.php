f72361e01719a6f50d8b2ec9aa083618b32616c1
unknown <chaow@hwtc.net>
2020-07-17 15:42:21 +0800

    Initial commit

---
+++ src/Math/Math.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chaow
+ * Date: 2020/7/17
+ * Time: 15:38
+ */
+
+namespace Math;
+
+
+class Math
+{
+    public function sum($a,$b){
+
+        return $a+$b;
+    }
+}
